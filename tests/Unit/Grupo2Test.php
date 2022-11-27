137e22ead24010de0238ba52612aecdadcd643ee
Jose Alberto Guevara Morales <59805803+CapiFrank@users.noreply.github.com>
2022-11-27 02:19:40 +0000

    Se soluciona conflicto del test

--- tests/Unit/Grupo2Test.php
+++ tests/Unit/Grupo2Test.php
@@ -14,6 +14,7 @@ use Tests\TestCase;
 
 class Grupo2Test extends TestCase
 {
+  use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -73,26 +74,26 @@ class Grupo2Test extends TestCase
 
   public function test_un_rol_puede_tener_usuarios()
   {
-    $rol = Role::make([
+    $rol = Role::create([
       'id'=>1,
       'type'=>'common_user'                
     ]);
-
-    $usuario = User::make([
+    
+    $usuario = User::create([
       'id'=>20,                  
-      'username'=>'asd',
-      'password'=>'asd',
+      'username'=>'asdasd',
+      'password'=>'asdasd',
       'first_name'=>'Jose',
       'second_name'=>'Alberto',
       'first_surname'=>'Guevara',
       'second_surname'=>'Morales',                   
-      'email'=>'5410jos@gmail.com',
+      'email'=>'catego@gmail.com',
       'email_verified_at'=>null,
       'role_id'=>1,
       'first_time'=>1                    
     ]);
 
-    $this->assertTrue($rol->id==$usuario->role_id);
+    $this->assertTrue($rol->id == $usuario->role_id);
   }
   
 }
